fe4d44ef4d609054ce168a6beea0dc9b2247fa6f
Madhurima <madhuui108@gmail.com>
2020-10-20 16:19:04 -0700

    Updates

---
+++ precache-manifest.e6570c40655d2aa41a6531a79ec1710c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16043a8099211a5d217394a4654bb739",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a8552897c35d77bcfe11",
+    "url": "/Portfolio/static/css/main.923b3d1d.chunk.css"
+  },
+  {
+    "revision": "4042acd549c2084fe298",
+    "url": "/Portfolio/static/js/2.fcb73e54.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Portfolio/static/js/2.fcb73e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8552897c35d77bcfe11",
+    "url": "/Portfolio/static/js/main.a1e29ddc.chunk.js"
+  },
+  {
+    "revision": "9bb5a14c4411ad556432",
+    "url": "/Portfolio/static/js/runtime-main.d985277c.js"
+  }
+]);
