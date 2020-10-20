4132cd7fe38f3843b771ddb4465258fd3304c4bc
Madhurima <madhuui108@gmail.com>
2020-10-20 16:56:13 -0700

    Added Profile Image

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <Link className="navbar-brand " to={process.env.PUBLIC_URL+"/"}>
                 <span className="d-block d-lg-none">Madhurima Gade</span>
-                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" /></span>
+                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2"  src="./madhu.JPG" alt="Madhu"/></span>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
